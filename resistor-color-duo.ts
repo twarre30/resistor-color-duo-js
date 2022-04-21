12dad43f63d9c9a4cb5a103d963c36e815f27042
Kyle Coberly <kyle.coberly@gmail.com>
2022-04-21 13:28:24 -0600

    Add solution

--- resistor-color-duo.ts
+++ resistor-color-duo.ts
@@ -1,1 +1,17 @@
-export function decodedValue() {}
+const COLORS = [
+  "black",
+  "brown",
+  "red",
+  "orange",
+  "yellow",
+  "green",
+  "blue",
+  "violet",
+  "grey",
+  "white",
+];
+
+const colorCode = (color: string): number => COLORS.indexOf(color);
+
+export const decodedValue = ([tens, ones]: string[]): number =>
+  colorCode(tens) * 10 + colorCode(ones);
