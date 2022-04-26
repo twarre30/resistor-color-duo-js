3da05304dfe02a432cb92fe43b43150b965567d2
Tiffany <twarre30@gmail.com>
2022-04-26 12:55:52 -0500

    solution

--- resistor-color-duo.js
+++ resistor-color-duo.js
@@ -1,1 +1,18 @@
-export function decodedValue() {}
+const colorCode = (color) => COLORS.indexOf(color);
+
+export const decodedValue = ([tens, ones]) =>
+  colorCode(tens) * 10 + colorCode(ones);
+
+
+const COLORS = [
+  "black",
+  "brown",
+  "red",
+  "orange",
+  "yellow",
+  "green",
+  "blue",
+  "violet",
+  "grey",
+  "white",
+];
